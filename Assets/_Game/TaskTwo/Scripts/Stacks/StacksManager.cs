f30690bf9b756ba850d0835e5b5c970e96658b13
sertacdncl <sertacdncl@hotmail.com>
2023-10-01 06:38:24 +0300

    Stacks refactored

--- Assets/_Game/TaskTwo/Scripts/Stacks/StacksManager.cs
+++ Assets/_Game/TaskTwo/Scripts/Stacks/StacksManager.cs
@@ -16,10 +16,6 @@ namespace TaskTwo.Stacks
 {
 	public class StacksManager : MonoBehaviour
 	{
-		public Transform stacksPoolParent;
-		public Transform lastPlacedStackTransform { get; set; }
-		public List<StackController> activeStacksList = new();
-
 		[Inject] public StackPoolService _stackPool;
 		[Inject] private LevelManager _levelManager;
 		[Inject] private StackSettingsData _stackSettingsData;
@@ -27,12 +23,17 @@ namespace TaskTwo.Stacks
 		private StackSettings Settings => _stackSettingsData.StackSettings;
 		private StackController CurrentStack => activeStacksList[^1];
 
-		private int _comboCounter = 0;
+		public Transform stacksPoolParent;
+		public Transform lastPlacedStackTransform { get; private set; }
+		public List<StackController> activeStacksList = new();
+
 		private Tween _stackMoveTween;
+		private int _comboCounter = 0;
 		private const float StackStartOffset = 10f;
 		private float GetRandomStackOffset => Random.Range(0, 1) == 1 ? -StackStartOffset : StackStartOffset;
-
 		private Transform _firstSpawnTransform;
+
+		//TODO: this part needs to be more clean and readable i guess make it better
 
 		private void OnEnable()
 		{
@@ -164,9 +165,19 @@ namespace TaskTwo.Stacks
 		private void CutStack()
 		{
 			StackController tempCurrentStack = CurrentStack;
-			Transform tempCurrentStackTransform = tempCurrentStack.transform;
-
 			StackController cutStack = CreateStackObject(false);
+
+			SetCutStackPosScale(tempCurrentStack, cutStack);
+			MakeFallStack(tempCurrentStack);
+			activeStacksList.Add(cutStack);
+			cutStack.gameObject.SetActive(true);
+
+			CheckCutStackSuccess(cutStack, tempCurrentStack);
+		}
+
+		private void SetCutStackPosScale(StackController tempCurrentStack, StackController cutStack)
+		{
+			Transform tempCurrentStackTransform = tempCurrentStack.transform;
 			Transform cutStackTransform = cutStack.transform;
 
 			cutStack.SetParent(_levelManager.currentLevelController.stacksParent);
@@ -188,10 +199,11 @@ namespace TaskTwo.Stacks
 			PlayComboSound(cutStack, tempCurrentStack);
 
 			//Current Stack
-			float factor = tempCurrentStackTransform.position.x - lastPlacedStackTransform.position.x > 0 ? 1 : -1;
+			float horizontalDirection =
+				tempCurrentStackTransform.position.x - lastPlacedStackTransform.position.x > 0 ? 1 : -1;
 			tempCurrentStackTransform.position = new Vector3(
 				(lastPlacedStackTransform.position.x + tempCurrentStackTransform.position.x) / 2f +
-				tempCurrentStackTransform.localScale.x * factor / 2f
+				tempCurrentStackTransform.localScale.x * horizontalDirection / 2f
 				, tempCurrentStackTransform.position.y
 				, tempCurrentStackTransform.position.z);
 
@@ -200,15 +212,11 @@ namespace TaskTwo.Stacks
 					lastPlacedStackTransform.localScale.x - cutStackTransform.localScale.x
 					, lastPlacedStackTransform.localScale.y
 					, lastPlacedStackTransform.localScale.z);
+		}
 
-
-			FallStack(tempCurrentStack);
-			activeStacksList.Add(cutStack);
-
-
-			cutStack.gameObject.SetActive(true);
-
-
+		private void CheckCutStackSuccess(StackController cutStack, StackController tempCurrentStack)
+		{
+			Transform tempCurrentStackTransform = tempCurrentStack.transform;
 			bool isStackPlaced = cutStack.transform.localScale.x > .05f;
 			if (isStackPlaced)
 			{
@@ -224,62 +232,11 @@ namespace TaskTwo.Stacks
 				cutStack.transform.position = tempCurrentStackTransform.position;
 				cutStack.transform.localScale = lastPlacedStackTransform.localScale;
 				tempCurrentStack.gameObject.SetActive(false);
-				FallStack(cutStack);
+				MakeFallStack(cutStack);
 			}
 		}
 
-		private StackController CutAndPlaceStack()
-		{
-			StackController cutStack = CreateStackObject(false);
-			Transform cutStackTransform = cutStack.transform;
-
-			StackController tempCurrentStack = CurrentStack;
-			Transform tempCurrentStackTransform = tempCurrentStack.transform;
-			Vector3 tempCurrentStackPos = tempCurrentStackTransform.position;
-			Vector3 tempCurrentStackLocalScale = tempCurrentStackTransform.localScale;
-			Vector3 lastPlacedStackPos = lastPlacedStackTransform.position;
-			Vector3 lastPlacedStackScale = lastPlacedStackTransform.localScale;
-
-			cutStack.SetParent(_levelManager.currentLevelController.stacksParent);
-			cutStack.SetMeshMaterial(tempCurrentStack.meshRenderer.material);
-
-			//Cut Stack
-			cutStackTransform.position = new Vector3(
-				(lastPlacedStackPos.x + tempCurrentStackPos.x) / 2f
-				, tempCurrentStackPos.y
-				, tempCurrentStackPos.z);
-
-			cutStackTransform.localScale = new Vector3(
-				tempCurrentStackLocalScale.x - Mathf.Abs(lastPlacedStackPos.x - tempCurrentStackPos.x)
-				, tempCurrentStackLocalScale.y
-				, tempCurrentStackLocalScale.z);
-			cutStack.gameObject.SetActive(true);
-
-			PlayComboSound(cutStack, tempCurrentStack);
-
-			//Current Stack
-			float factor = tempCurrentStackPos.x - lastPlacedStackPos.x > 0 ? 1 : -1;
-			tempCurrentStackTransform.position = new Vector3(
-				(lastPlacedStackPos.x + tempCurrentStackPos.x) / 2f + tempCurrentStackLocalScale.x * factor / 2f
-				, tempCurrentStackPos.y
-				, tempCurrentStackPos.z);
-
-			tempCurrentStackTransform.localScale =
-				new Vector3(
-					lastPlacedStackScale.x - cutStackTransform.localScale.x
-					, lastPlacedStackScale.y
-					, lastPlacedStackScale.z);
-
-			activeStacksList.Remove(CurrentStack);
-			activeStacksList.Add(cutStack);
-
-			FallStack(tempCurrentStack);
-			cutStack.gameObject.SetActive(true);
-
-			return cutStack;
-		}
-
-		private void FallStack(StackController stackController)
+		private void MakeFallStack(StackController stackController)
 		{
 			stackController.moveTween?.Kill();
 			stackController.rigidbody.mass = 100f;
@@ -287,27 +244,6 @@ namespace TaskTwo.Stacks
 			stackController.ToggleRigidbodyKinematic(false);
 			activeStacksList.Remove(stackController);
 			DOVirtual.DelayedCall(1f, () => _stackPool.ReturnStackToPool(stackController));
-		}
-
-		#endregion
-
-		private void PlayComboSound(StackController cutStack, StackController tempCurrentStack)
-		{
-			bool failThreshold = cutStack.transform.localScale.x > .05f;
-			var isGoodCut = cutStack.transform.localScale.x / tempCurrentStack.transform.localScale.x > .9f;
-			if (isGoodCut)
-			{
-				_comboCounter++;
-				_audioManager.PlayNote(0.5f + (_comboCounter * .1f));
-			}
-			else
-			{
-				_comboCounter = 1;
-				if (failThreshold)
-					_audioManager.PlayNote(0.5f);
-				else
-					_audioManager.PlayFail();
-			}
 		}
 
 		private void CreateStackPlaceEffect()
@@ -330,6 +266,27 @@ namespace TaskTwo.Stacks
 			);
 
 			fakeStack.gameObject.SetActive(true);
+		}
+
+		#endregion
+
+		private void PlayComboSound(StackController cutStack, StackController tempCurrentStack)
+		{
+			bool failThreshold = cutStack.transform.localScale.x > .05f;
+			var isGoodCut = cutStack.transform.localScale.x / tempCurrentStack.transform.localScale.x > .9f;
+			if (isGoodCut)
+			{
+				_comboCounter++;
+				_audioManager.PlayNote(0.5f + (_comboCounter * .1f));
+			}
+			else
+			{
+				_comboCounter = 1;
+				if (failThreshold)
+					_audioManager.PlayNote(0.5f);
+				else
+					_audioManager.PlayFail();
+			}
 		}
 	}
 }
